6f24ff71883b21061931d48c5ba51e1615e30bba
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-12-15 19:47:09 +0100

    Update exclass.py

--- exclass.py
+++ exclass.py
@@ -13,14 +13,19 @@ class Composition:
     def getquantite(self):
         return self.__quantite
 
-    def produit(self, new_produit):
+    def setproduit(self, new_produit):
         self.__produit = new_produit
 
-    def quantite(self, new_quantite):
+    def setquantite(self, new_quantite):
         self.__quantite = new_quantite
 
     def __str__(self):
-        return f"{self.__quantite} x {str(self.__produit)}"
+        return f"{self.__quantite} :{str(self.__produit)}"
+    
+    def __eq__(self,a)  :
+        return self.__produit==a.getproduit and self.__quantite==a.getquantite
+        
+
 
 
 
@@ -41,9 +46,12 @@ class Produit(ABC):
     def getPrixHT(self):
         pass
 
-
-
-
+    def __str__(self):
+        return f"{self.getnom} {str(self.getcode)}: "
+    def __eq__(self,a)  :
+        return self.__nom==a.getnom and self.__code==a.getcode
+    
+     
 class ProduitElementaire(Produit):
     def __init__(self, nom, code, prixAchat):
         super().__init__(nom, code)
@@ -51,9 +59,12 @@ class ProduitElementaire(Produit):
 
     def __str__(self):
         return f"{self.getnom} {str(self.getcode)}: {str(self.getPrixHT())} dh"
-
+    
     def getPrixHT(self):
         return self.__prixAchat
+    
+    def setPrixHT (self,n):
+        self.__PrixHT=n
 
 
 class ProduitCompose(Produit):
@@ -63,7 +74,7 @@ class ProduitCompose(Produit):
         super().__init__(nom, code)
         self.__fraisFabrication = fraisFabrication
         self.__listeConstituants = listeConstituants
-
+    
     @property
     def fraisFabrication(self):
         return self.__fraisFabrication
@@ -84,17 +95,17 @@ class ProduitCompose(Produit):
 
 
 
-# Example 
-p1 = ProduitElementaire("P1", "001", 10.0)
-p2 = ProduitElementaire("P2", "002", 15.0)
+# Example usage
+cpu = ProduitElementaire("cpu", "1", 1010)
+ram = ProduitElementaire("ram", "2", 600)
 
-composition_p3 = [Composition(p1, 2), Composition(p2, 4)]
-composition_p4 = [Composition(p2, 3), Composition(p1, 2)]
 
-p3 = ProduitCompose("P3", "003", 5.0, composition_p3)
-p4 = ProduitCompose("P4", "004", 8.0, composition_p4)
+composition_p4 = [Composition(ram, 1), Composition(cpu,1)]
 
-print(p1)
-print(p2)
-print(p3)
+
+p4 = ProduitCompose("pc", "003",100, composition_p4)
+
+print(cpu)
+print(ram)
+
 print(p4)
